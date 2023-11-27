15c3d903f90dd7114ccab8843286a3b048fe444f
Ellinor Teurnberg <eteu@kth.se>
2023-11-27 11:52:45 +0100

    start på di-gamma och gamma

--- Lab2 - HMMs/HMM3/HMM3.py
+++ Lab2 - HMMs/HMM3/HMM3.py
@@ -4,7 +4,9 @@ class Baum_Welch():
  
     def __init__(self):
         self.readInput()
+        self.alphaPass()
         self.betaPass()
+        self.gamma()
         self.maxIters = 10 #godtyckligt vald, har ingen aning
         self.iters = 0
         self.oldLogProb = float("-inf")
@@ -54,6 +56,15 @@ class Baum_Welch():
     
         return result
 
+    def multiplyMatrixWithVector(self, M, v):
+        result = []
+        for i in range(len(M)):
+            result.append(0)
+            for j in range(len(M[0])):
+                result[i] += M[i][j] * v[j]
+        
+        return result
+
     def multiplyMatrices(self, X, Y):
         result = [[0 for x in range(len(Y[0]))] for y in range(len(X))]
         # iterate through rows of X
@@ -67,35 +78,75 @@ class Baum_Welch():
         return result
 
 
-    def alphaPass(self):
-        alpha = []
-        alpha.append(self.elementWiseProduct(self.pi, self.transpose(self.B)[self.sequence[0]]))
+    """def alphaPass(self):
+        self.alpha = []
+        self.alpha.append(self.elementWiseProduct(self.pi, self.transpose(self.B)[self.sequence[0]]))
         for i in range(1, self.M):
             A_T = self.transpose(self.A)
-            alpha.append(self.elementWiseProduct(self.multiplyMatrixWithVector(A_T, alpha[i - 1]), self.transpose(self.B)[self.sequence[i]]))
+            self.alpha.append(self.elementWiseProduct(self.multiplyMatrixWithVector(A_T, self.alpha[i - 1]), self.transpose(self.B)[self.sequence[i]]))
 
-        return sum(alpha[self.M-1]) 
+        #return alpha """
+    
+    def alphaPass(self):
+        self.alpha = [[]]
+        self.c = [0]
+
+        #compute alpha 0(i)
+        for i in range(len(self.A)):
+            pi = -math.inf if self.pi[i] == 0 else math.log(self.pi[i])
+            b = -math.inf if self.B[self.sequence[0]][i] == 0 else math.log(self.B[self.sequence[0]][i])
+            self.alpha[0].append(pi + b)
+            self.c[0] += self.alpha[0][i]
+
+        #Scale the alpha 0 (i)
+        self.c[0] = math.log(1 / self.c[0])
+        for i in range(len(self.A)):
+            self.alpha[0][i] = 
+
            
     def betaPass(self):
-        beta = [[]]
+        self.beta = [[]]
 
         #Timestep T
         for i in range(len(self.A)):
-            beta[0].append(1)
+            self.beta[0].append(1)
 
         for t in range(len(self.sequence)-1):
-            beta.insert(0,[])
+            self.beta.insert(0,[])
             for i in range(len(self.A)): #current states
                 sum = 0
                 for j in range(len(self.A)): #states after
 
-                    nextVal = beta[1][j] # Next beta value
+                    nextVal = self.beta[1][j] # Next beta value
                     transVal = -math.inf if self.A[i][j] == 0.0 else math.log(self.A[i][j])
                     obsVal = -math.inf if self.B[j][self.sequence[i]] == 0.0 else math.log(self.B[j][self.sequence[i]])
                     sum += nextVal + transVal + obsVal
-                beta[0].append(sum)
+                self.beta[0].append(sum)
 
-            print(t)
+
+    def di_gamma(self):
+        di_gamma = [] #Ev göra om till self? Och ropa på i const.
+        denom = math.log(sum(self.alpha[self.M-1]))
+        for t in range(len(self.sequence)):
+            di_gamma.append([])
+            for i in range(len(self.A)):
+                di_gamma[t].append([])
+                for j in range(len(self.A)):
+                    nom = -math.inf if self.alpha[t][i] == 0.0 else math.log(self.alpha[t][i])
+                    nom += -math.inf if self.A[j][i] == 0.0 else math.log(self.A[j][i])
+                    nom += -math.inf if self.B[j][self.sequence[t+1]] == 0.0 else math.log(self.B[j][self.sequence[t+1]])
+                    nom += self.beta[t+1][j]
+                    di_gamma[t][i].append(nom / denom) 
+
+    def gamma(self):
+        di_gamma = self.di_gamma() 
+        self.gamma = [] #Ev göra om till self? Och ropa på i const.
+        for t in range(len(self.sequence)):
+            self.gamma.append([])
+            for i in range(len(self.A)):
+                self.gamma[t].append(sum(di_gamma[t][i]))
+
+
 
     
 test = Baum_Welch()
